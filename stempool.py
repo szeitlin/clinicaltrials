b1207cd546d8fed26fcdcc4481a62741341787fc
Samantha Zeitlin <samanthazeitlin@gmail.com>
2016-08-12 12:04:32 -0700

    refactored to simplify. Just create sets of stems and combine them.

--- stempool.py
+++ stempool.py
@@ -5,7 +5,7 @@ import itertools
 from collections import Counter
 
 from nltk.stem.snowball import SnowballStemmer
-from rest_query import CLI, DirectQuery
+from rest_query import DirectQuery
 from nlp_helpers import apply_word_tokenize
 
 class StemPool:
@@ -14,27 +14,49 @@ class StemPool:
         self.dq = DirectQuery(term)
         self.stemmer = SnowballStemmer('english')
         self.df = self.dq.df
+        self.stemset = self.fill_stempool()
 
-    def fill_stempool(self, df):
+    def fill_stempool(self):
         """
         Tokenize, flatten, stem, and count.
 
         :param df: pandas DataFrame of names and unique ids
         :return: self.stemcounts - Counter (dict of {stem (str): count(int)})
         """
-        tokens = [apply_word_tokenize(x) for x in df['name']]
+        tokens = [apply_word_tokenize(x) for x in self.df['name']]
 
         flatten1 = itertools.chain.from_iterable
         flat = list(flatten1(tokens))
 
         stems = [self.stemmer.stem(x) for x in flat]
-        self.stemcounts = Counter(stems)
+        #self.stemcounts = Counter(stems) #do we care about counts?
+        return set(stems)
 
-    def inventory_check(self):
-        """
-        Check whether terms are already in the stempool.
-        If not, add them.
+    # def inventory_check(self, term):
+    #     """
+    #     Check whether a new term is already in the stempool.
+    #     If not, add it.
+    #
+    #     note to self: may want to vectorize this later
+    #
+    #     :return: updated stemcounts
+    #     """
+    #     if self.stemcounts.get(term) is not None:
+    #         self.stemcounts[term] +=1
+    #     else:
+    #         self.stemcounts.update(term)
 
-        :return: updated stemcounts
-        """
-        pass
+
+def stemset_combiner(stempool1, stempool2):
+    """
+    Merge stempools from two sets of terms.
+
+    question: do we care about counts right now? Maybe not.
+
+    :param stempool1: (set of str)
+    :param stempool2: (set of str)
+    :return: bigger stempool
+    """
+    return stempool1.stemset | stempool2.stemset
+
+#want to group related stems - not sure how to do this yet
--- test_stempool.py
+++ test_stempool.py
@@ -2,7 +2,7 @@ __author__ = 'szeitlin'
 
 import pandas as pd
 import unittest
-from stempool import StemPool
+from stempool import StemPool, stemset_combiner
 
 @unittest.skip("deprecated in favor of DQ protocol")
 class TestCLIStemPool(unittest.TestCase):
@@ -25,14 +25,30 @@ class TestDQStemPool(unittest.TestCase):
 
     def setUp(cls):
         cls.pool = StemPool("breast")
+        cls.pool2 = StemPool("cyst")
 
     def test_df_exists(self):
         self.assertTrue(isinstance(self.pool.df, pd.DataFrame))
         self.assertEqual(list(self.pool.df.columns), ['name', 'ui'])
 
     def test_fill_stempool(self):
+        self.assertIn('breast', self.pool.stemset)
+
+    @unittest.skip("deprecated")
+    def test_inventory_check(self):
         self.pool.fill_stempool(self.pool.df)
-        self.assertIn('breast', self.pool.stemcounts)
+        self.pool.inventory_check('lymphoma')
+        self.assertNotIn('lymphoma', self.pool.stemcounts)
+        pre = self.pool.stemcounts.get('cyst')
+        self.pool.inventory_check('cyst')
+        post = self.pool.stemcounts.get('cyst')
+        self.assertGreater(post, pre)
+
+    def test_stemset_combiner(self):
+        self.assertTrue(isinstance(self.pool2.stemset, set))
+        bigger = stemset_combiner(self.pool, self.pool2)
+        self.assertTrue(isinstance(bigger, set))
+        self.assertGreater(len(bigger), len(self.pool.stemset))
 
 if __name__=='__main__':
     unittest.main()
